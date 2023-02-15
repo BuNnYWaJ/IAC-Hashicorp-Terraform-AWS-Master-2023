df6dc6fa12da682c8cd89068c6b3fd8d9f79a06e
Bharathwaj-KS <iambharathwaj.suresh@gmail.com>
2023-02-15 11:12:56 +0530

    Welcome to BuNnYWaJ

--- git-deploy.sh
+++ git-deploy.sh
@@ -2,7 +2,7 @@
 
 echo "Add files and do local commit"
 git add .
-git commit -am "Welcome to BharathWaj-KS_StackSimplify"
+git commit -am "Welcome to TechWorld_With_BuNnY!"
 
 echo "Pushing to Github Repository"
 git push
